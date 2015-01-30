496a3cf89e1c769fb1b12a1344c2118bceea6944
Chung Tran <huychungtran@gmail.com>
2015-01-30 22:57:36 +0900

    Adds user firsts specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
-require 'api_contraints'
+require 'api_constraints'
 
 MarketPlaceApi::Application.routes.draw do
+  devise_for :users
   #Api definition
-  namespace :api, defaults: { format:json }, constraints: { subdomain:'api'},path:'/'  do 
-      scope module: :v1, constraints: ApiConstrains.new(version: 1, default: true) do
+  namespace :api, defaults: { format: 'json' }, constraints: { subdomain:'api'},path:'/'  do 
+      scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
           #listing resource route here
       end
   end
